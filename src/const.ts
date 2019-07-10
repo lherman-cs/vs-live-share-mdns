a65fdc24e19f86801299888abddb9a3b92a97c21
Lukas <lherman.cs@gmail.com>
2019-07-10 10:33:58 -0700

    Added some scripts and dependencies, and changed publish timeout to be 30 secs

--- src/const.ts
+++ src/const.ts
@@ -4,5 +4,5 @@ import { userInfo } from "os";
 export const userName = userInfo().username;
 export const serviceName = "liveShare";
 
-export const publishTimeout = 15; // in seconds
+export const publishTimeout = 30; // in seconds
 export const discoveryTimeout = 10; // in seconds
